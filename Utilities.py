a3412b6a2df274defd74530b6cede6d7219deaa0
Herber Roman Alvarado Vasquez <roman.alvaradohn.edu@gmail.com>
2024-03-01 20:07:28 -0600

    Inicio de unir todo

---
+++ Utilities.py
@@ -0,0 +1,38 @@
+############################################################################
+#CONTROL DE EXCEPCIONES
+############################################################################
+class Utilities():
+
+    #Manejador de errores de socket =======================================
+    def error_handler(self, e):
+        msj = ""
+
+        if(type(e) is KeyboardInterrupt):
+            msj = "Script terminado por teclado"
+
+        elif(type(e) is ValueError):
+            msj = "Usuario abandonó"
+
+        elif(type(e) is OSError):
+            msj = "Direccion en uso. Utilize: ss -ltpn | grep [server_port]"
+
+        elif(type(e) is ConnectionRefusedError):
+            msj = "Conexion rechazada. Valide que el servidor este activo y a la escucha"
+
+        elif(type(e) is mariadb.Error):
+            msj = "Error con la base de datos:\n{e}"
+
+        else:
+            msj = f"Error: {type(e)}\n{e}"
+
+        self.clear_console()
+        print(msj + "\n\n")
+        exit()
+
+
+    #Limpiar pantalla =====================================================
+    def clear_console(self):
+        if os.name == 'nt':  # Windows
+            os.system('cls')
+        else:  # Linux, Unix, macOS, POSIX
+            os.system('clear')
